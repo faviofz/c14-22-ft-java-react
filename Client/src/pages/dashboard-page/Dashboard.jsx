cf26fbf642c07586de5d20d3e3346a8119339735
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2023-10-17 10:31:27 -0300

    improvement(FE): fixed size

--- Client/src/pages/dashboard-page/Dashboard.jsx
+++ Client/src/pages/dashboard-page/Dashboard.jsx
@@ -9,134 +9,112 @@ import {
 import './dashboard-page.scss';
 import { Stat } from '../../components/stat-cmp/Stat';
 import { Link } from 'react-router-dom';
+import DetailModal from '../../components/detail-modal-cmp/DetailModal';
 
 export function Dashboard() {
   return (
     <div>
       <Navbar />
-      <section className='flex flex-row flex-wrap gap-5 m-5'>
-        <div className='flex flex-row flex-wrap gap-5'>
+      <section className='flex flex-col items-center justify-center gap-5 mx-5 mt-5'>
+        <div className='flex flex-row flex-wrap items-center justify-center gap-5'>
           <Stat title='Productos' stat={31} icon={IconProductSVG} />
           <Stat title='Stock' stat={1.2} icon={DocumentReportSVG} />
           <Stat title='Proveedor' stat={5} icon={TruckSVG} />
           <Stat title='Historial' stat={5.8} icon={RefreshSVG} />
         </div>
 
-        <div className='flex flex-col justify-between w-full gap-5 p-5 h-[30rem] bg-base-200 rounded-3xl'>
-          <div>
-            <div className='flex gap-5'>
-              <img
-                width={20}
-                height={20}
-                src={IconProductSVG}
-                alt='icon product'
-              />
-              <h1>Últimos productos registrados</h1>
-            </div>
+        <DetailModal />
 
-            <div className='flex justify-between'>
-              <div className='flex gap-5'>
-                <input
-                  type='checkbox'
-                  className='checkbox checkbox-primary checkbox-sm'
-                />
-                <h1>Producto</h1>
-              </div>
-              <h1 className='tracking-wide text-primary'>Categoria</h1>
-            </div>
-
-            <div className='flex justify-between'>
-              <div className='flex gap-5'>
-                <input
-                  type='checkbox'
-                  className='checkbox checkbox-primary checkbox-sm'
-                />
-                <h1>Producto</h1>
-              </div>
-              <h1 className='tracking-wide text-primary'>Categoria</h1>
-            </div>
-
-            <div className='flex justify-between'>
-              <div className='flex gap-5'>
-                <input
-                  type='checkbox'
-                  className='checkbox checkbox-primary checkbox-sm'
-                />
-                <h1>Producto</h1>
-              </div>
-              <h1 className='tracking-wide text-primary'>Categoria</h1>
-            </div>
-
-            <div className='flex justify-between'>
-              <div className='flex gap-5'>
-                <input
-                  type='checkbox'
-                  className='checkbox checkbox-primary checkbox-sm'
-                />
-                <h1>Producto</h1>
-              </div>
-              <h1 className='tracking-wide text-primary'>Categoria</h1>
-            </div>
-
-            <div className='flex justify-between'>
-              <div className='flex gap-5'>
-                <input
-                  type='checkbox'
-                  className='checkbox checkbox-primary checkbox-sm'
-                />
-                <h1>Producto</h1>
-              </div>
-              <h1 className='tracking-wide text-primary'>Categoria</h1>
-            </div>
-          </div>
-
-          <div className='flex flex-col gap-3 mt-5'>
-            <button className='w-full btn btn-outline btn-primary'>
-              Ver más productos
-            </button>
-            <button className='w-full btn btn-primary'>Agregar nuevo</button>
-          </div>
-        </div>
-
-        <div className='flex flex-col w-full gap-5 p-5 bg-base-200 rounded-3xl'>
+        <div className='flex flex-col w-[27rem] sm:w-[33rem] md:w-[45rem] h-[30rem] gap-5 p-5 bg-base-200 justify-between rounded-3xl'>
           <div className='flex gap-5'>
             <img width={20} height={20} src={BellSVG} alt='icon product' />
             <h1>Notificaciones</h1>
           </div>
 
-          <div className='flex justify-between'>
-            <h1>Notificacion 1</h1>
-            <Link to='#' className='tracking-wide text-primary'>
-              Ver detalle
-            </Link>
-          </div>
+          <div className='flex flex-col gap-3 overflow-y-auto'>
+            <div className='flex justify-between'>
+              <h1>Notificacion 1</h1>
+              <Link to='#' className='tracking-wide text-primary'>
+                Ver detalle
+              </Link>
+            </div>
 
-          <div className='flex justify-between'>
-            <h1>Notificacion 1</h1>
-            <Link to='#' className='tracking-wide text-primary'>
-              Ver detalle
-            </Link>
-          </div>
+            <div className='flex justify-between'>
+              <h1>Notificacion 1</h1>
+              <Link to='#' className='tracking-wide text-primary'>
+                Ver detalle
+              </Link>
+            </div>
 
-          <div className='flex justify-between'>
-            <h1>Notificacion 1</h1>
-            <Link to='#' className='tracking-wide text-primary'>
-              Ver detalle
-            </Link>
-          </div>
+            <div className='flex justify-between'>
+              <h1>Notificacion 1</h1>
+              <Link to='#' className='tracking-wide text-primary'>
+                Ver detalle
+              </Link>
+            </div>
 
-          <div className='flex justify-between'>
-            <h1>Notificacion 1</h1>
-            <Link to='#' className='tracking-wide text-primary'>
-              Ver detalle
-            </Link>
-          </div>
+            <div className='flex justify-between'>
+              <h1>Notificacion 1</h1>
+              <Link to='#' className='tracking-wide text-primary'>
+                Ver detalle
+              </Link>
+            </div>
 
-          <div className='flex justify-between'>
-            <h1>Notificacion 1</h1>
-            <Link to='#' className='tracking-wide text-primary'>
-              Ver detalle
-            </Link>
+            <div className='flex justify-between'>
+              <h1>Notificacion 1</h1>
+              <Link to='#' className='tracking-wide text-primary'>
+                Ver detalle
+              </Link>
+            </div>
+
+            <div className='flex justify-between'>
+              <h1>Notificacion 1</h1>
+              <Link to='#' className='tracking-wide text-primary'>
+                Ver detalle
+              </Link>
+            </div>
+
+            <div className='flex justify-between'>
+              <h1>Notificacion 1</h1>
+              <Link to='#' className='tracking-wide text-primary'>
+                Ver detalle
+              </Link>
+            </div>
+
+            <div className='flex justify-between'>
+              <h1>Notificacion 1</h1>
+              <Link to='#' className='tracking-wide text-primary'>
+                Ver detalle
+              </Link>
+            </div>
+
+            <div className='flex justify-between'>
+              <h1>Notificacion 1</h1>
+              <Link to='#' className='tracking-wide text-primary'>
+                Ver detalle
+              </Link>
+            </div>
+
+            <div className='flex justify-between'>
+              <h1>Notificacion 1</h1>
+              <Link to='#' className='tracking-wide text-primary'>
+                Ver detalle
+              </Link>
+            </div>
+
+            <div className='flex justify-between'>
+              <h1>Notificacion 1</h1>
+              <Link to='#' className='tracking-wide text-primary'>
+                Ver detalle
+              </Link>
+            </div>
+
+            <div className='flex justify-between'>
+              <h1>Notificacion 1</h1>
+              <Link to='#' className='tracking-wide text-primary'>
+                Ver detalle
+              </Link>
+            </div>
           </div>
 
           <div className='flex flex-col gap-3 mt-5'>
