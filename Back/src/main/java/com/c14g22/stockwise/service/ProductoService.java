43851548a174d5aea0f7945e1c823ae8c04ab5b0
faviofz <favioo.wow@gmail.com>
2023-10-18 19:24:51 -0300

    Api Rest Producto

---
+++ Back/src/main/java/com/c14g22/stockwise/service/ProductoService.java
@@ -0,0 +1,19 @@
+package com.c14g22.stockwise.service;
+
+import com.c14g22.stockwise.dto.ProductoDto;
+import com.c14g22.stockwise.model.Producto;
+import java.util.List;
+
+
+public interface ProductoService {
+
+  public List<Producto> obtenerProductos();
+
+  public Producto obtenerProductoPorId(Long id);
+
+  public Producto guardarProducto(Producto producto);
+
+  public void actualizarProducto(Long id, ProductoDto productoDto);
+
+  public void borrarProducto(Long id);
+}
