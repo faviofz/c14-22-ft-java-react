fb56db15a07dc7f4553a257f1a9cc871c36841e8
faviofz <favioo.wow@gmail.com>
2023-10-19 20:59:25 -0300

    Add RestServiceCorsConfig configuration class

---
+++ Back/src/main/java/com/c14g22/stockwise/RestServiceCorsConfig.java
@@ -0,0 +1,17 @@
+package com.c14g22.stockwise;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class RestServiceCorsConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
+  }
+}
