6e20c30cdc82ebfae5c8b0b1e857cc5115e07e87
faviofz <favioo.wow@gmail.com>
2023-10-20 01:24:19 -0300

    Fix PedidoRepository

--- Back/src/main/java/com/c14g22/stockwise/repository/PedidoRepository.java
+++ Back/src/main/java/com/c14g22/stockwise/repository/PedidoRepository.java
@@ -1,26 +1,13 @@
 package com.c14g22.stockwise.repository;
 
 import com.c14g22.stockwise.model.Pedido;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface PedidoRepository extends CrudRepository<Pedido, Long> {
-
-
-    Iterable<Pedido> findAll();
-
-    // Obtiene un pedido por su ID
-    Optional<Pedido> findById(Long id);
-
-    // Crea un nuevo pedido
-    Pedido save(Pedido pedido);
-
-    // Actualiza un pedido existente
-    Pedido update(Pedido pedido);
-
-    // Elimina un pedido
-    void deleteById(Long id);
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 }
