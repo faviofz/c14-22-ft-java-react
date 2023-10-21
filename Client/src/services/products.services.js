44cb78452951eafd4006b9098c7186ba9a07bca5
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-10-20 20:56:04 -0500

    feat(improvement) Create productServices structure

---
+++ Client/src/services/products.services.js
@@ -0,0 +1,33 @@
+// CREATE
+export function createProduct(product) {
+  return new Promise((resolve, reject) => {
+    throw new Error('Implement createProduct');
+  });
+}
+
+// READ
+export function getAllProducts() {
+  return new Promise((resolve, reject) => {
+    throw new Error('Implement getAllProducts');
+  });
+}
+
+export function getProductById(id) {
+  return new Promise((resolve, reject) => {
+    throw new Error('Implement getProductById');
+  });
+}
+
+// UPDATE
+export function updateProductById(id) {
+  return new Promise((resolve, reject) => {
+    throw new Error('Implement getProductById');
+  });
+}
+
+// DELETE
+export function deleteProductById(id) {
+  return new Promise((resolve, reject) => {
+    throw new Error('Implement getProductById');
+  });
+}
