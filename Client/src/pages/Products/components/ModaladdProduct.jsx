0a909efb0cad0a9e93f11ea84f2415db03dd1c2a
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2023-10-21 12:22:33 -0300

    improvement(FE): fixed mobile size

--- Client/src/pages/Products/components/ModaladdProduct.jsx
+++ Client/src/pages/Products/components/ModaladdProduct.jsx
@@ -8,99 +8,94 @@ export function ModaladdProduct() {
           onClick={() => document.getElementById('my_modal_3').showModal()}
           className='w-full gap-5 md:w-80 lg:w-52 sm:btn-wide btn btn-primary'
         >
-          <PlusIcon width="15" />
+          <PlusIcon width='15' />
           Nuevo Producto
         </button>
 
-        <dialog id='my_modal_3' className='modal '>
-          <div className='w-11/12 max-w-5xl modal-box '>
-            <form method='dialog'>
-              <div className='flex items-center mb-5 justify-evenly'>
-                <h3 className="text-slate-800 text-[28px] font-semibold font-['Inter']">
+        <dialog id='my_modal_3' className='modal'>
+          <div className='w-full h-full modal-box'>
+            <form
+              method='dialog'
+              className='flex flex-col justify-between h-full'
+            >
+              {/* TITLE */}
+              <div className='flex items-center justify-between '>
+                <h3 className='text-3xl font-semibold text-secondary '>
                   Nuevo Producto
                 </h3>
-                <button className='text-[30px] hover:text-red-600'>x</button>
+                <button className='absolute btn btn-md btn-circle btn-ghost right-3 top-3'>
+                  ✕
+                </button>
               </div>
 
-              <section className='flex justify-center gap-5'>
-                <section className='flex flex-col gap-4'>
-                  <div>
-                    <label>Nombre</label>
-                    <input
-                      type='text'
-                      placeholder='Type here'
-                      className='w-full max-w-xs input input-bordered'
-                    />
+              {/* FORM */}
+              <section className='flex flex-col gap-5'>
+                <div>
+                  <label>Nombre</label>
+                  <input
+                    type='text'
+                    placeholder='Type here'
+                    className='w-full input input-bordered'
+                  />
+                </div>
+                <div>
+                  <label>Provedor</label>
+                  <input
+                    type='text'
+                    placeholder='Type here'
+                    className='w-full input input-bordered'
+                  />
+                </div>
+                <div className='flex flex-col'>
+                  <label>Precio</label>
+                  <input
+                    type='number'
+                    placeholder='0'
+                    className='w-full input input-bordered'
+                  />
+                </div>
+                <div>
+                  <label>Fecha</label>
+                  <input
+                    type='date'
+                    placeholder='Type here'
+                    className='w-full input input-bordered'
+                  />
+                </div>
+                <div>
+                  <label>Marca</label>
+                  <input
+                    type='text'
+                    placeholder='Type here'
+                    className='w-full input input-bordered'
+                  />
+                </div>
+                <div>
+                  <label>Imagen</label>
+                  <div className=' h-[146px] bg-base-200 border-2 border-accent'>
+                    <div className='w-8 h-8 left-[139px] top-[56px] absolute' />
                   </div>
-                  <div>
-                    <label>Provedor</label>
-                    <input
-                      type='text'
-                      placeholder='Type here'
-                      className='w-full max-w-xs input input-bordered'
-                    />
-                  </div>
-                  <div className='flex gap-4'>
-                    <div className='flex flex-col'>
-                      <label>Precio</label>
-                      <input
-                        type='number'
-                        placeholder='Type here'
-                        className='input input-bordered w-[60px] max-w-xs'
-                      />
-                    </div>
-                    <div>
-                      <label>fecha</label>
-                      <input
-                        type='date'
-                        placeholder='Type here'
-                        className='w-full max-w-xs input input-bordered'
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <label>Marca</label>
-                    <input
-                      type='text'
-                      placeholder='Type here'
-                      className='w-full max-w-xs input input-bordered'
-                    />
-                  </div>
-                </section>
-                <section className='flex flex-col gap-5'>
-                  <div>
-                    <label>Imagen</label>
-                    <div className='w-[309px] h-[146px] relative bg-gray-100 border-2 border-slate-600'>
-                      <div className='w-8 h-8 left-[139px] top-[56px] absolute' />
-                    </div>
-                  </div>
-                  <div>
-                    <input
-                      type='file'
-                      className='w-full max-w-xs file-input file-input-bordered file-input-info'
-                    />
-                  </div>
-                  <div>
-                    <label>Categoria</label>
-                    <input
-                      type='text'
-                      placeholder='Type here'
-                      className='w-full max-w-xs input input-bordered'
-                    />
-                  </div>
-                </section>
+                </div>
+                <div>
+                  <input
+                    type='file'
+                    className='w-full file-input file-input-bordered file-input-primary'
+                  />
+                </div>
+                <div>
+                  <label>Categoria</label>
+                  <input
+                    type='text'
+                    placeholder='Type here'
+                    className='w-full input input-bordered'
+                  />
+                </div>
               </section>
-              <div className='flex items-center justify-center gap-6 mt-5 mr-3'>
-                <div>
-                  <button className='btn btn-outline btn-info w-[295px]'>
-                    Cancelar
-                  </button>
-                </div>
-                <div>
-                  <button className='btn btn-info text-white bg-blue-500 w-[320px]'>
-                    Guardar
-                  </button>
-                </div>
+
+              {/* BUTTOMS */}
+              <div className='flex justify-between'>
+                  <button className='w-[12rem] btn btn-outline btn-primary'>Cancelar</button>
+                  <button className='w-[12rem] btn btn-primary'>Guardar</button>
               </div>
             </form>
           </div>
