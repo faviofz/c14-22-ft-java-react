2c638db8c0f59dce0741aa6127f06d6ba7829a21
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-10-25 17:01:43 -0500

    fix(fe): Fix alert component styles

--- Client/src/components/alert-cmp/Alert.jsx
+++ Client/src/components/alert-cmp/Alert.jsx
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types';
 import { InfoIcon, SuccessIcon, ErrorIcon, WarningIcon } from './svg';
-
+import './alert.scss';
 export function Alert({ type, message = '' }) {
+  const classNameList = [
+    'alert-component alert',
+    type ? `alert-${type}` : '',
+  ].join(' ');
   return (
     <>
       {message ? (
-        <div className={`alert alert-${type}`}>
+        <div className={classNameList}>
           {type === 'info' && <InfoIcon />}
           {type === 'success' && <SuccessIcon />}
           {type === 'error' && <ErrorIcon />}
