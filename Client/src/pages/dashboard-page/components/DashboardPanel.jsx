8901253058230e2643cf2ab7de6e587891f8000d
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-10-24 23:15:48 -0500

    ui(fe): Change some icon colors

--- Client/src/pages/dashboard-page/components/DashboardPanel.jsx
+++ Client/src/pages/dashboard-page/components/DashboardPanel.jsx
@@ -4,7 +4,7 @@ export function DashboardPanel({ children, title, Icon, listItems }) {
   return (
     <div className='dashboard-page-panel flex flex-col justify-between gap-5 p-5 h-[25rem] bg-base-200 rounded-3xl w-full'>
       <div className='flex gap-5'>
-        <Icon className='w-5 h-5' />
+        <Icon className='w-5 h-5  [&>path]:stroke-secondary-content' />
         <h1>{title}</h1>
       </div>
       <div className='flex flex-col items-start h-full gap-3 overflow-y-auto'>
