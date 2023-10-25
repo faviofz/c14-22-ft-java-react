eb3d9b9e47c8473e67128d0a04ebf1ecd46c6f60
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-10-25 01:13:46 -0500

    feat(fe): Add form validation to login

--- Client/src/pages/login-page/Login.jsx
+++ Client/src/pages/login-page/Login.jsx
@@ -1,43 +1,59 @@
-import { Panel, Input, Button, Footer, Logo } from '@/components';
+import { Panel, Input, Button, Footer, Logo, Alert } from '@/components';
 import { DoubleColumnLayout } from '@/layout';
 import { Link } from 'react-router-dom';
 import { useAuth } from '@/hooks/useAuth';
 import './login-page.scss';
+import { useEffect, useState } from 'react';
+
+const initialFormValues = {
+  userName: '',
+  password: '',
+};
 
 export default function Login() {
   const { authState, onLogin } = useAuth();
+  const [formValues, setFormValue] = useState(initialFormValues);
+
+  const handleChange = ({ target }) => {
+    setFormValue(s => ({ ...s, [target.name]: target.value }));
+  };
 
   const handleSubmit = e => {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const formValues = Object.fromEntries(formData);
     onLogin(formValues);
   };
+
+  useEffect(() => {
+    setFormValue(initialFormValues);
+  }, [authState.errorMessage]);
 
   return (
     <div className='login-page'>
       <DoubleColumnLayout double={true}>
         <DoubleColumnLayout.Left>
           <Panel title='Acceda a su cuenta'>
-            {authState?.errorMessage}
-
+            <Alert message={authState?.errorMessage} type='error' />
             <form onSubmit={handleSubmit}>
               <Input
-                label='Correo electrónico'
-                placeholder='Ingresa tu correo electrónico'
+                label='Nombre de usuario'
+                placeholder='Ingresa tu nombre de usuario'
                 name='userName'
+                onChange={handleChange}
+                value={formValues.userName}
               />
               <Input
                 label='Contraseña'
                 placeholder='Ingresa tu contraseña'
                 name='password'
+                onChange={handleChange}
+                value={formValues.password}
+                type='password'
               />
               <Button disabled={authState.loading}>
-                {authState.loading ? 'Enviando...' : 'Ingresar'}
+                {authState?.loading ? 'Enviando...' : 'Ingresar'}
               </Button>
             </form>
             <Link to='/forgot-password'>¿Olvidaste tu contraseña?</Link>
-            {JSON.stringify(authState, null, 2)}
           </Panel>
           <div>
             ¿No tiene una cuenta? <Link to='/register'>Regístrese</Link>
