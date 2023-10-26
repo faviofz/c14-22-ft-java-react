837fda8d6a9a07c5f391d0845f7540ab0c6572ea
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-10-26 07:34:36 -0500

    feat(fe): Add skeleton component

---
+++ Client/src/components/skeleton-cmp/TableSkeleton.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import { useId } from 'react';
+import './skeleton.scss';
+
+export function TableSkeleton({ rows = 5, headers }) {
+  const id = useId();
+  const itemsRows = new Array(rows).fill('');
+
+  return (
+    <table className='skeleton table bg-base-200'>
+      <thead>
+        <tr>
+          {headers.map((item, index) => (
+            <th key={id + index + 'th'}>{item}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {itemsRows.map((item, i) => (
+          <tr key={id + i + 'td'}>
+            {headers.map((item, index) => (
+              <td key={id + index + 'td'}>
+                <div className='skeleton-table-item'></div>
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+TableSkeleton.propTypes = {
+  rows: PropTypes.number,
+  headers: PropTypes.array,
+};
