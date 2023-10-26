ebedddbd72302c94df65d21b1f4009db30fe33c1
faviofz <favioo.wow@gmail.com>
2023-10-25 22:05:44 -0300

    Fix Categorias

--- Back/src/main/java/com/c14g22/stockwise/controller/CategoriaController.java
+++ Back/src/main/java/com/c14g22/stockwise/controller/CategoriaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categorias")
+@RequestMapping
 public class CategoriaController {
 
     private final CategoriaRepository categoriaRepository;
