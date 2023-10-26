08c2f0c90b6a81e6c852cacc2513280fccc496cc
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2023-10-25 19:37:23 -0500

    feat(fe): Create useCategories hook

---
+++ Client/src/hooks/useCategories.jsx
@@ -0,0 +1,43 @@
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  getAllCategoriesAsync,
+  getCategoryAsync,
+  createCategoryAsync,
+  updateCategoryAsync,
+  deleteCategoryAsync,
+} from '@/redux/reducers';
+
+export function useCategories() {
+  const { categories, loading } = useSelector(state => state.categories);
+  const dispatch = useDispatch();
+
+  function getAllCategories() {
+    dispatch(getAllCategoriesAsync());
+  }
+
+  function getCategory(id) {
+    dispatch(getCategoryAsync(id));
+  }
+
+  function createCategory(newCategory) {
+    dispatch(createCategoryAsync(newCategory));
+  }
+
+  function updateCategory(modifiedCategory) {
+    dispatch(updateCategoryAsync(modifiedCategory));
+  }
+
+  function deleteCategory(id) {
+    dispatch(deleteCategoryAsync(id));
+  }
+
+  return {
+    categories,
+    loading,
+    getAllCategories,
+    getCategory,
+    createCategory,
+    updateCategory,
+    deleteCategory,
+  };
+}
