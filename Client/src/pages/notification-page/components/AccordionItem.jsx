a19310ba413dbb7062b506cb1fcac37146958a27
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2023-11-03 18:00:00 -0300

    fix

--- Client/src/pages/notification-page/components/AccordionItem.jsx
+++ Client/src/pages/notification-page/components/AccordionItem.jsx
@@ -1,15 +1,18 @@
 import PropType from 'prop-types';
 import { Link } from 'react-router-dom';
 
-export const AccordionItem = ({title, description}) => (
+export const AccordionItem = ({title, description1, description2}) => (
   <div className='collapse collapse-arrow bg-base-200 min-w-[26rem] max-w-[45rem]  w-full focus-within:bg-primary focus-within:text-white [&>div>a]:focus-within:text-white'>
     <input type='radio' name='my-accordion-2'  />
     <div className='text-xl font-medium collapse-title'>
       {title}
     </div>
-    <div className='flex justify-between px-8 collapse-content'>
-      <p>{description}</p>
-      <Link to={"/product/addProducts"}>Agregar Productos</Link> 
+    <div className='flex items-center justify-between px-8 collapse-content'>
+      <div className='flex flex-col'>
+      <p>{description1}</p>
+      <p>{description2}</p>
+      </div>
+      <Link className='btn btn-primary btn-sm' to={"/product/addProducts"}>Agregar Productos</Link> 
     </div>
    
   </div>
@@ -17,7 +20,8 @@ export const AccordionItem = ({title, description}) => (
 
 AccordionItem.propType = {
     title: PropType.string,
-    description: PropType.string,
+    description1: PropType.string,
+    description2: PropType.string,
 }
 
 AccordionItem.defaultProps = {
