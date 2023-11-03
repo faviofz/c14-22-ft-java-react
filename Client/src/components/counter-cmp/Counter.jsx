2bc5c7faf818f1982a9ecfdaf48630dc90d4eca1
Araya Alejo <82895574+alejoaraya@users.noreply.github.com>
2023-11-03 17:09:18 -0300

    fix

--- Client/src/components/counter-cmp/Counter.jsx
+++ Client/src/components/counter-cmp/Counter.jsx
@@ -2,16 +2,14 @@ import PropTypes from 'prop-types';
 import { Input } from '@/components';
 import './counter-cmp.scss';
 
-export function Counter({ handler, currentValue, id, actual}) {
+export function Counter({ handler, currentValue, id }) {
   const handleChange = ({ target }) => {
     if (Number(target.value) < 0) return;
     handler(id)(target.value);
   };
 
   const handleClick = num => () => {
-    console.log(`${(Number(currentValue) + num)} menor a ${actual}`)
     if (Number(currentValue) + num < 0) return;
-    if((Number(currentValue) + num) > actual) return;
     handler(id)(String(Number(currentValue) + num));
   };
 
@@ -36,5 +34,4 @@ Counter.propType = {
   handler: PropTypes.func,
   currentValue: PropTypes.string,
   id: PropTypes.number,
-  actual: PropTypes.number,
 };
